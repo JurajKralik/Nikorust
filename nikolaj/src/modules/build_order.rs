e62c84c939535ad540f45cf4df629908848c68fe
JurajKralik <juraj.kralik95@gmail.com>
2024-05-15 00:48:43 +0200

    working on tech requirements

--- nikolaj/src/modules/build_order.rs
+++ nikolaj/src/modules/build_order.rs
@@ -1,7 +1,9 @@
+use std::collections::HashMap;
+
 use crate::Nikolaj;
 use rust_sc2::prelude::*;
 
-pub(crate) fn decide_build_order(bot: &mut Nikolaj) -> Vec<UnitTypeId>{
+pub(crate) fn decide_build_order(bot: &mut Nikolaj) -> Vec<UnitTypeId> {
     const BIO: &'static [UnitTypeId] = &[
         UnitTypeId::Marine,
         UnitTypeId::Marauder,
@@ -39,7 +41,168 @@ pub(crate) fn decide_build_order(bot: &mut Nikolaj) -> Vec<UnitTypeId>{
 }
 
 pub(crate) fn execute_build_order(bot: &mut Nikolaj) {
+    let unit_source: HashMap<UnitTypeId, UnitTypeId> = [
+        (UnitTypeId::Marine, UnitTypeId::Barracks),
+        (UnitTypeId::Marauder, UnitTypeId::Barracks),
+        (UnitTypeId::Reaper, UnitTypeId::Barracks),
+        (UnitTypeId::Ghost, UnitTypeId::Barracks),
+        (UnitTypeId::Hellion, UnitTypeId::Factory),
+        (UnitTypeId::WidowMine, UnitTypeId::Factory),
+        (UnitTypeId::Cyclone, UnitTypeId::Factory),
+        (UnitTypeId::SiegeTank, UnitTypeId::Factory),
+        (UnitTypeId::Thor, UnitTypeId::Factory),
+        (UnitTypeId::VikingFighter, UnitTypeId::Starport),
+        (UnitTypeId::Banshee, UnitTypeId::Starport),
+        (UnitTypeId::Liberator, UnitTypeId::Starport),
+        (UnitTypeId::Raven, UnitTypeId::Starport),
+        (UnitTypeId::Medivac, UnitTypeId::Starport),
+        (UnitTypeId::Battlecruiser, UnitTypeId::Starport),
+    ]
+    .iter()
+    .cloned()
+    .collect();
+
     let build_order = decide_build_order(bot);
     for unit in build_order {
+        if let Some(source) = unit_source.get(&unit) {
+            if tech_requirements_met(bot, unit) {
+                
+            }
+        }
+    }
+}
+
+fn tech_requirements_met(bot: &Nikolaj, unit: UnitTypeId) -> bool {
+    match unit {
+        UnitTypeId::Marine => !bot
+            .units
+            .my
+            .structures
+            .of_type_including_alias(UnitTypeId::Barracks)
+            .ready()
+            .is_empty(),
+        UnitTypeId::Marauder => !bot
+            .units
+            .my
+            .structures
+            .of_type_including_alias(UnitTypeId::Barracks)
+            .ready()
+            .is_empty(),
+        UnitTypeId::Reaper => !bot
+            .units
+            .my
+            .structures
+            .of_type_including_alias(UnitTypeId::Barracks)
+            .ready()
+            .is_empty(),
+        UnitTypeId::Ghost => {
+            !bot.units
+                .my
+                .structures
+                .of_type_including_alias(UnitTypeId::Barracks)
+                .ready()
+                .is_empty()
+                && !bot
+                    .units
+                    .my
+                    .structures
+                    .of_type(UnitTypeId::GhostAcademy)
+                    .ready()
+                    .is_empty()
+        }
+        UnitTypeId::Hellion => !bot
+            .units
+            .my
+            .structures
+            .of_type_including_alias(UnitTypeId::Factory)
+            .ready()
+            .is_empty(),
+        UnitTypeId::WidowMine => !bot
+            .units
+            .my
+            .structures
+            .of_type_including_alias(UnitTypeId::Factory)
+            .ready()
+            .is_empty(),
+        UnitTypeId::Cyclone => !bot
+            .units
+            .my
+            .structures
+            .of_type_including_alias(UnitTypeId::Factory)
+            .ready()
+            .is_empty(),
+        UnitTypeId::SiegeTank => !bot
+            .units
+            .my
+            .structures
+            .of_type_including_alias(UnitTypeId::Factory)
+            .ready()
+            .is_empty(),
+        UnitTypeId::Thor => {
+            !bot.units
+                .my
+                .structures
+                .of_type_including_alias(UnitTypeId::Factory)
+                .ready()
+                .is_empty()
+                && !bot
+                    .units
+                    .my
+                    .structures
+                    .of_type(UnitTypeId::Armory)
+                    .ready()
+                    .is_empty()
+        }
+        UnitTypeId::VikingFighter => !bot
+            .units
+            .my
+            .structures
+            .of_type_including_alias(UnitTypeId::Starport)
+            .ready()
+            .is_empty(),
+        UnitTypeId::Banshee => !bot
+            .units
+            .my
+            .structures
+            .of_type_including_alias(UnitTypeId::Starport)
+            .ready()
+            .is_empty(),
+        UnitTypeId::Liberator => !bot
+            .units
+            .my
+            .structures
+            .of_type_including_alias(UnitTypeId::Starport)
+            .ready()
+            .is_empty(),
+        UnitTypeId::Raven => !bot
+            .units
+            .my
+            .structures
+            .of_type_including_alias(UnitTypeId::Starport)
+            .ready()
+            .is_empty(),
+        UnitTypeId::Medivac => !bot
+            .units
+            .my
+            .structures
+            .of_type_including_alias(UnitTypeId::Starport)
+            .ready()
+            .is_empty(),
+        UnitTypeId::Battlecruiser => {
+            !bot.units
+                .my
+                .structures
+                .of_type_including_alias(UnitTypeId::Starport)
+                .ready()
+                .is_empty()
+                && !bot
+                    .units
+                    .my
+                    .structures
+                    .of_type(UnitTypeId::FusionCore)
+                    .ready()
+                    .is_empty()
+        }
+        _ => false,
     }
 }
