d5180a0cbc8ae55fbf1de41e5f9cbe0570c0065f
juraj <juraj.kralik95@gmail.com>
2025-09-12 20:57:54 +0200

    debugging added

--- nikolaj/src/main.rs
+++ nikolaj/src/main.rs
@@ -1,5 +1,5 @@
 #![allow(non_snake_case)]
-use rust_sc2::{bot, prelude::*};
+use rust_sc2::{bot, debug::Debugger, geometry::*, prelude::*};
 
 mod ex_main;
 mod helpers;
@@ -18,6 +18,7 @@ use crate::units::scv::*;
 #[bot]
 #[derive(Default)]
 struct Nikolaj {
+    debugger: Debugger,
     iteration: usize,
     worker_allocator: WorkerAllocator,
     scanner_sweep_time: f32,
@@ -83,14 +84,47 @@ impl Nikolaj {
         self.counter().ordered().count(unit_type)
     }
 
-    #[allow(dead_code)]
     fn unit_count(&self, unit_type: UnitTypeId) -> usize {
         self.units.my.units.of_type(unit_type).len()
     }
 
-    #[allow(dead_code)]
     fn structure_count(&self, unit_type: UnitTypeId) -> usize {
         self.units.my.structures.ready().of_type(unit_type).len()
+    }
+
+    fn get_debug_color(&self, color: &str) -> Option<(u32, u32, u32)> {
+        match color {
+            "Red" => Some((255, 0, 0)),
+            "Green" => Some((0, 255, 0)),
+            "Blue" => Some((0, 0, 255)),
+            "Cyan" => Some((0, 255, 255)),
+            "Yellow" => Some((255, 255, 0)),
+            "Magenta" => Some((255, 0, 255)),
+            "White" => Some((255, 255, 255)),
+            "Black" => Some((0, 0, 0)),
+            _ => Some((255, 255, 255)),
+        }
+    }
+    fn debug_cube(&mut self, center: Point3, size: f32, color: &str) {
+        let debug_color = self.get_debug_color(color);
+        self.debugger.draw_cube(center, size, debug_color);
+    }
+    fn debug_sphere(&mut self, center: Point3, radius: f32, color: &str) {
+        let debug_color = self.get_debug_color(color);
+        self.debugger.draw_sphere(center, radius, debug_color);
+    }
+    fn debug_line(&mut self, start: Point3, end: Point3, color: &str) {
+        let debug_color = self.get_debug_color(color);
+        self.debugger.draw_line(start, end, debug_color);
+    }
+    fn debug_text(&mut self, text: &str, pos: Point3, color: &str, size: Option<u32>) {
+        let debug_color = self.get_debug_color(color);
+        self.debugger.draw_text_world(text, pos, debug_color, size);
+    }
+    fn debug_text_screen(&mut self, text: &str, pos: Point2, color: &str, size: Option<u32>) {
+        let debug_color = self.get_debug_color(color);
+        let position = Some((pos.x, pos.y));
+        self.debugger.draw_text_screen(text, position, debug_color, size);
     }
 }
 
