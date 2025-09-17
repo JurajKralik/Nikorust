45933fe4161b92c446b9fcf5d6d0a48534ca7064
Juraj <juraj.kralik95@gmail.com>
2025-09-17 13:59:22 +0200

    light targeting calculation

--- nikolaj/src/units/marine.rs
+++ nikolaj/src/units/marine.rs
@@ -5,4 +5,7 @@ use crate::units::surroundings::*;
 
 pub fn marine_control(bot: &mut Nikolaj, unit: &Unit) {
     let surroundings = get_surroundings_info(bot, unit);
+    if let Some(target) = surroundings.best_target_in_range {
+        unit.attack(Target::Tag(target.tag()), false);
+    }
 }
--- nikolaj/src/units/surroundings.rs
+++ nikolaj/src/units/surroundings.rs
@@ -3,22 +3,115 @@ use rust_sc2::prelude::*;
 
 
 pub struct SurroundingsInfo {
-    pub current_target: Option<Unit>,
-    pub better_target: Option<Unit>,
+    pub best_target_in_range: Option<Unit>,
+    pub better_target_off_range: Option<Unit>,
     pub closest_threat: Option<Unit>,
     pub closest_counter: Option<Unit>,
+    pub in_danger: bool,
+}
+impl Default for SurroundingsInfo {
+    fn default() -> Self {
+        SurroundingsInfo {
+            best_target_in_range: None,
+            better_target_off_range: None,
+            closest_threat: None,
+            closest_counter: None,
+            in_danger: false,
+        }
+    }
 }
 
 pub fn get_surroundings_info(bot: &mut Nikolaj, unit: &Unit) -> SurroundingsInfo {
-    let mut surroundings = SurroundingsInfo {
-        current_target: None,
-        better_target: None,
-        closest_threat: None,
-        closest_counter: None,
-    };
+    let mut surroundings = SurroundingsInfo::default();
+    let enemies = bot.units.enemy.units.clone();
+    let sorted_enemies = enemies.iter().sort_by_distance(unit.position());
+    for enemy in sorted_enemies {
+        // Check threat
+        if can_attack(enemy, unit) {
+            if surroundings.closest_threat.is_none() {
+                surroundings.closest_threat = Some(enemy.clone());
+            }
+            if in_range(enemy, unit) {
+                surroundings.in_danger = true;
+            }
+        }
+        // Targeting
+        if can_attack(unit, enemy) {
+            if in_range(unit, enemy) {
+                // First
+                if surroundings.best_target_in_range.is_none() {
+                    surroundings.best_target_in_range = Some(enemy.clone());
+                // Better
+                } else {
+                    surroundings.best_target_in_range = Some(better_of_targets(unit, &surroundings.best_target_in_range.clone().unwrap(), enemy));
+                }
+            } else {
+                // First
+                if surroundings.best_target_in_range.is_none() && surroundings.better_target_off_range.is_none() {
+                    surroundings.better_target_off_range = Some(enemy.clone());
+                // Better from off range
+                } else if surroundings.better_target_off_range.is_some() {
+                    surroundings.better_target_off_range = Some(better_of_targets(unit, &surroundings.better_target_off_range.clone().unwrap(), enemy));
+                // Better than in range
+                } else {
+                    let better_target = Some(better_of_targets(unit, &surroundings.best_target_in_range.clone().unwrap(), enemy));
+                    if better_target.clone().unwrap().tag() != surroundings.best_target_in_range.clone().unwrap().tag() {
+                        surroundings.better_target_off_range = better_target;
+                    }
+                }
+            }
+        }
+    }
+    
+
 
 
 
 
     surroundings
+}
+fn can_attack(attacker: &Unit, target: &Unit) -> bool {
+    if target.is_flying() && attacker.can_attack_air() {
+        return true;
+    }
+    if !target.is_flying() && attacker.can_attack_ground() {
+        return true;
+    }
+    false
+}
+fn in_range(attacker: &Unit, target: &Unit) -> bool {
+    let distance = attacker.position().distance(target.position());
+    if target.is_flying() {
+        if distance <= attacker.air_range() {
+            return true;
+        }
+    } else {
+        if distance <= attacker.ground_range() {
+            return true;
+        }
+    }
+    false
+}
+fn better_of_targets(attacker: &Unit, current_target: &Unit, new_target: &Unit) -> Unit {
+    let current_target_damage = attacker.real_range_vs(current_target);
+    let current_target_health = (current_target.health().unwrap_or(0) + current_target.shield().unwrap_or(0)) as f32;
+    let current_target_dies = current_target_health - current_target_damage <= 0.0;
+    let new_target_damage = attacker.real_range_vs(new_target);
+    let new_target_health = (new_target.health().unwrap_or(0) + new_target.shield().unwrap_or(0)) as f32;
+    let new_target_dies = new_target_health - new_target_damage <= 0.0;
+    let higher_damage_target = if current_target_damage >= new_target_damage { current_target } else { new_target };
+    let lower_health_target = if current_target_health <= new_target_health { current_target } else { new_target };
+
+    if current_target_dies {
+        if new_target_dies {
+            return higher_damage_target.clone();
+        } else {
+            return current_target.clone();
+        }
+    } else if new_target_dies {
+        return new_target.clone();
+    } else if current_target_damage != new_target_damage {
+        return higher_damage_target.clone();
+    }
+    lower_health_target.clone()
 }
