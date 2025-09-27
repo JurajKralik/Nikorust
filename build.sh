438a1e96343806f151a78ee350029cb2707ac510
juraj <juraj.kralik95@gmail.com>
2025-09-27 12:55:17 +0200

    build script

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e  # exit on error
+
+# Go to repo
+cd /home/dax/Bob/Repositories/Nikorust
+
+# Build with musl target
+cargo build --release --target=x86_64-unknown-linux-musl
+
+# Paths
+BUILD_DIR="target/x86_64-unknown-linux-musl/release"
+BIN_SRC="$BUILD_DIR/Nikolaj"
+BIN_DEST="$BUILD_DIR/RustyNikolaj"
+ZIP_NAME="RustyNikolaj.zip"
+
+# Rename binary
+cp "$BIN_SRC" "$BIN_DEST"
+
+# Create zip
+cd "$BUILD_DIR"
+zip -r "$ZIP_NAME" "RustyNikolaj"
+
+# Move zip to /
+mv "$ZIP_NAME" /home/dax/Bob/Repositories/Nikorust
